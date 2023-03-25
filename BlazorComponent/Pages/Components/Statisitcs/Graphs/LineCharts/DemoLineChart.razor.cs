f5aa1ae1b91fd446c8f4643320e2dc05e856dbc6
StefansTechLog <socials@stefanstechlog.com>
2023-03-25 20:44:49 +0100

    Scoped javascript example

---
+++ BlazorComponent/Pages/Components/Statisitcs/Graphs/LineCharts/DemoLineChart.razor.cs
@@ -0,0 +1,58 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace BlazorComponent.Pages.Components.Statisitcs.Graphs.LineCharts
+{
+    public partial class DemoLineChart
+    {
+        [Inject] IJSRuntime? JSRuntime { get; set; }
+
+        private IJSObjectReference? JsModule { get; set; }
+
+
+        private int MaxDataPoints { get; set; } = 250;
+        private int RerenderFrequency { get; set; } = 5;
+
+        private CancellationTokenSource? CancellationTokenSource;
+
+        private int x = 0;
+
+        protected override async Task OnInitializedAsync()
+        {
+            JsModule = await JSRuntime!.InvokeAsync<IJSObjectReference>("import", "/Pages/Components/Statisitcs/Graphs/LineCharts/DemoLineChart.razor.js");
+            await JsModule!.InvokeVoidAsync("InitializeChart", RerenderFrequency, MaxDataPoints);
+        }
+
+        private async Task UpdateMaxDataPoints()
+        {
+            await JsModule!.InvokeVoidAsync("UpdateMaxDataPoints", MaxDataPoints);
+        }
+        private async Task UpdateRerenderFrequency()
+        {
+            await JsModule!.InvokeVoidAsync("UpdateRerenderFrequency", RerenderFrequency);
+        }
+
+        private async Task StartStream()
+        {
+            CancellationTokenSource = new CancellationTokenSource();
+            while (!CancellationTokenSource!.IsCancellationRequested)
+            {
+                await JsModule!.InvokeVoidAsync("AddDataPoint", x++, Random.Shared.Next(130, 140));
+                await Task.Delay(10);
+            }
+        }
+
+        private void StopStream()
+        {
+            CancellationTokenSource?.Cancel();
+        }
+
+        private async Task ResetStream()
+        {
+            CancellationTokenSource?.Cancel();
+            await JsModule!.InvokeVoidAsync("ResetDataPoints");
+        }
+
+
+    }
+}
---
+++ BlazorComponent/Pages/Components/Statisitcs/Graphs/LineCharts/DemoLineChart.razor.js
@@ -0,0 +1,64 @@
+﻿import  'https://cdn.jsdelivr.net/npm/chart.js';
+
+var _chart;
+var _renderTimer;
+var _maxDataPoints;
+
+export function InitializeChart(rerenderFrequency, maxDataPoints) {
+    var chart_element = document.getElementById('demo-chart');
+
+    _chart = new Chart(chart_element, {
+        type: 'line',
+        data: {
+            labels: [],
+            datasets: [
+                {
+                    label: 'live data graph',
+                    data: [],
+                    borderWidth: 1
+                }
+            ]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: false
+                }
+            },
+            animation: false
+        }
+    });
+
+    UpdateRerenderFrequency(rerenderFrequency);
+    _maxDataPoints = maxDataPoints;
+}
+
+export function UpdateMaxDataPoints(maxDataPoints) {
+    _maxDataPoints = maxDataPoints;
+    _chart.data.labels = _chart.data.labels.slice(-(_maxDataPoints));
+    _chart.data.datasets[0].data = _chart.data.datasets[0].data.slice(-(_maxDataPoints));
+}
+
+export function UpdateRerenderFrequency(rerenderFrequency) {
+    try {
+        clearInterval(_renderTimer)
+    } catch (ex) {
+        console.log(ex);
+    }
+
+    _renderTimer = setInterval(() => {
+        _chart.update();
+    }, rerenderFrequency);
+}
+
+export function AddDataPoint(label, value) {
+    _chart.data.labels.push(label);
+    _chart.data.datasets[0].data.push(value);
+    _chart.data.labels = _chart.data.labels.slice(-(_maxDataPoints));
+    _chart.data.datasets[0].data = _chart.data.datasets[0].data.slice(-(_maxDataPoints));
+}
+
+export function ResetDataPoints() {
+    _chart.data.labels = [];
+    _chart.data.datasets[0].data = [];
+}
